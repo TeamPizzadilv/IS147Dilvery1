0e34d061e1a5d8d8aeace363154160ae05a02ce8
zykia.tinkler@gmail.com <zykia.tinkler@gmail.com>
2025-03-03 20:23:18 -0500

    i am chaningws

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello,World");
         System.out.println("zykia");
-        System.out.println("hsjdh");
-
+        System.out.println("hello");
+        System.out.println("this is practice 2");
+    }
 }
