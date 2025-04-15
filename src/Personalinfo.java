8a41c08fcd646d200238c5a28e286dccebfc6317
zykia.tinkler@gmail.com <zykia.tinkler@gmail.com>
2025-04-14 21:31:45 -0400

    changes

---
+++ src/Personalinfo.java
@@ -0,0 +1,11 @@
+/**
+ * This Personal info class is to handle all of the customers personal info, includin
+ * @author Zykia Tinkler
+ */
+public class Personalinfo {
+
+    /**
+     * this gets the customers name
+     */
+    private String name;
+}
