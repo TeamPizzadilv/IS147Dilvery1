763ce5c00336f2004ff1607a41a5969eebdb861a
ZyKia Tinkler <zykia.tinkler@gmail.com>
2025-04-14 21:06:36 -0400

    Update Main.java
    javadocs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+ /**
+         * This a main class to print and call all methods found
+         * @author Zykia Tinkler
+         */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello,World");
