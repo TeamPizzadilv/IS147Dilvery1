07884338844e20e998467be2e0affe323e8a4437
zykia.tinkler@gmail.com <zykia.tinkler@gmail.com>
2025-04-21 19:33:08 -0400

    new changes

--- src/Personalinfo.java
+++ src/Personalinfo.java
@@ -2,6 +2,8 @@
  * This Personal info class is to handle all of the customers personal info, includin
  * @author Zykia Tinkler
  */
+// this should ask for the persons name, their address, if they are a returning customer.
+    // if they are a returning customer then go to another file called returning
 public class Personalinfo {
 
     /**
