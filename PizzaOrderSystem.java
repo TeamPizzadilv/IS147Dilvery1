19666c8402006b816a75f8c1cd77953b678fe784
LandoTate22 <landonS1@umbc.edu>
2025-05-05 11:59:26 -0400

    Update PizzaOrderSystem.java

--- PizzaOrderSystem.java
+++ PizzaOrderSystem.java
@@ -1,2 +1,29 @@
+import java.util.*;
+
 public class PizzaOrderSystem {
+
+    static Scannner scanner = new Scanner(System.in);
+    static String customerAddress;
+    static List<String> orderitems = new ArrayList<>();
+    static double totalCost = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to IS147 Pizza Shop!");
+        enterAddress();
+        selectPizza();
+        selectSides();
+        selectDessertsandDrinks();
+        checkout();
+    }
+
+    public static void enterAddress() {
+        System.out.print("Please enter your address");
+        customerAddress = scanner.nextLine();
+    }
+
+    //Methods for later steps
+    public static void selectPizza() {}
+    public static void selectSides() {}
+    public static void selectDessertsandDrinks() {}
+    public static void checkout() {}
 }
