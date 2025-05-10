4c4b02e7b433ffb5bb8e286bff66073dcd2a3009
Landon Smith <landons1@umbc.edu>
2025-05-09 21:15:26 -0400

    Pizza Size Update

--- src/PizzaSize.java
+++ src/PizzaSize.java
@@ -1,5 +1,26 @@
 // This file should be use to ask what size pizza the customer wants. Should be called in the main
 //This should extend from the pizza file. Should be asked what pizza they want then what size the want
 //Should offer small, medium or large pizza
-public class PizzaSize {
-}
+public static void selectPizza() {}
+        System.out.println("\n--- Pizza Customization---"");
+
+                           //selecting the size
+                           System.out.println("Choose pizza size:\n1. Small ($8)\n2. Medium ($10")\n3. Large ($12)");
+int sizeChoice = scanner.nextInt();
+        scanner.nextLine();
+        switch (sizeChoice)
+
+        {
+        case 1:
+        orderItems.add("Small Pizza");
+totalCost += 8;
+        break;
+        case 2
+        orderItems.add("Medium Pizza");
+totalCost += 10;
+        break;
+        case 3
+        orderItems.add("Large Pizza");
+totalCost += 12;
+        break;
+        }
